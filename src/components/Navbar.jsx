ffa8dc1474cf7cb28a829a3d4dbcd62cffa24b00
RomiRao <raoromina96@gmail.com>
2023-12-08 15:33:22 -0300

    adding drawer, changing icons, everything is responsive because i'm very cool

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import { AppBar, Breadcrumbs, Link, Toolbar } from "@mui/material";
+import { AppBar, Breadcrumbs, IconButton, Link, Toolbar, Drawer } from "@mui/material";
+import { useState } from "react";
+//icons
 import MenuIcon from '@mui/icons-material/Menu';
+import NavListDrawer from './NavListDrawer'
+import HomeIcon from '@mui/icons-material/Home';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 export default function Navbar() {
+
+  const [open, setOpen] = useState(false);
 
   const navLinks = [
     {
@@ -20,19 +27,32 @@ export default function Navbar() {
   }
 
   return (
+    <>
     <AppBar sx={{bgcolor: 'transparent', boxShadow: 'none'}} position="sticky">
       <Toolbar sx={{justifyContent: 'space-between'}}>
-
-        <Breadcrumbs separator="/" aria-label="breadcrumb">
+        <DarkModeIcon color='primary'/>
+        <Breadcrumbs separator="/" aria-label="breadcrumb" sx={{display: {xs: 'none', sm: 'flex'}}}>
           {navLinks.filter(item => item.title != 'Home').map((item) => (
             <Link underline="hover" key={item.title} color="inherit" href={item.path} onClick={(e) => handleClick(e)}>
               {item.title}
             </Link>
           ))}
         </Breadcrumbs>
-
-        <MenuIcon color="black"/>
+        <HomeIcon color='primary' sx={{display: {xs: 'none', sm: 'flex'}}}/>
+        <IconButton onClick={() => setOpen(true)} sx={{display: {xs: 'flex', sm: 'none'}}}>
+          <MenuIcon color="primary" />
+        </IconButton>
       </Toolbar>
     </AppBar>
+      <Drawer
+      anchor="right"
+      open={open}
+      onClose={() => setOpen(false)}
+      sx={{display: {xs: 'flex', sm: 'none'}}}
+      >
+          <NavListDrawer setOpen={setOpen} />
+      </Drawer>
+   </>
   )
 }
+//
