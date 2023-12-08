9809e980f29d76d64c0624602c4be097dbbf01b4
RomiRao <raoromina96@gmail.com>
2023-12-08 17:27:06 -0300

    i know that my commits arent enough so i decided to stop just to say i made a beautiful nav on the bottom on the page, keep scrolling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,35 @@
+import { Toolbar, IconButton, Breadcrumbs, Box, Typography } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <Box py={3}>
+      <Toolbar sx={{justifyContent: 'center'}}>
+        <Breadcrumbs separator="" aria-label="breadcrumb" >
+          <IconButton component='a'>
+            <LinkedInIcon color="primary" />
+          </IconButton>
+          <IconButton component='a'>
+            <InstagramIcon color="primary" />
+          </IconButton>
+          <IconButton component='a'>
+            <GitHubIcon color="primary" />
+          </IconButton>
+          <IconButton component='a'>
+            <EmailIcon color="primary" />
+          </IconButton>
+        </Breadcrumbs>
+      </Toolbar>
+      <Typography variant="caption" sx={{display: 'flex', justifyContent: 'center'}}>
+        The One Piece is real.
+      </Typography>
+    </Box>
   )
 }
+
+//sx={{display: {xs: 'none', sm: 'flex'}}}
+//onClick={(e) => handleClick(e)}
