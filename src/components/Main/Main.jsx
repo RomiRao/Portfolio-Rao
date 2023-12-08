2174b1b49427c136837bc1a1c9e73f5d1ba81eac
RomiRao <raoromina96@gmail.com>
2023-12-08 18:39:52 -0300

    adding container component to main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
+import { Container } from '@mui/material'
 import Profile from  './Profile.jsx'
 
 export default function Main() {
   return (
-    <>
+    <Container sx={{display: 'flex', justifyContent: 'space-between'}}>
       <Profile/>
-    </>
+    </Container>
   )
 }
