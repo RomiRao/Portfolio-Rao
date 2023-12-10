143f6b60ef98ec10f857e5f9ec1c4d51155f7911
RomiRao <raoromina96@gmail.com>
2023-12-10 14:44:15 -0300

    deleting projectcard component and putting the code inside projects component

--- src/components/Main/Projects.jsx
+++ src/components/Main/Projects.jsx
@@ -1,5 +1,11 @@
 import { Box } from "@mui/material";
-import ProjectCard from "./ProjectCard";
+//card components
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default function Projects() {
 
@@ -43,7 +49,27 @@ export default function Projects() {
 
   return (
     <Box display='flex' justifyContent='center' flexWrap='wrap' py={5}>
-      {projects.map((project) => <ProjectCard key={project.title} title={project.title} img={project.img} description={project.description} repository={project.repository} deploy={project.deploy} />)}
+      {projects.map((project) => (
+      <Card key={project.title} sx={{ maxWidth: 300, m: 3, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
+            sx={{ height: 150 }}
+            image={project.img}
+            title={project.title}
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {project.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                {project.description}
+            </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" href={project.repository} target="_blank">Repository</Button>
+                <Button size="small" href={project.deploy} target="_blank">Deploy</Button>
+            </CardActions>
+      </Card>
+      ))}
     </Box>
   )
 }
