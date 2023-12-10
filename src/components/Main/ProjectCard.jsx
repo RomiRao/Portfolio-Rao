4119ed780893a28dff6fba645c3c813f1259c774
RomiRao <raoromina96@gmail.com>
2023-12-10 13:16:29 -0300

    card structure done

--- src/components/Main/ProjectCard.jsx
+++ src/components/Main/ProjectCard.jsx
@@ -1,5 +1,30 @@
-export default function ProjectCard() {
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function ProjectCard({title, img, description, repository, deploy}) {
   return (
-    <div>ProjectCard</div>
+    <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+        sx={{ height: 140 }}
+        image={img}
+        title={title}
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            {description}
+        </Typography>
+        </CardContent>
+        <CardActions>
+            <Button size="small" href={repository} target="_blank">Repository</Button>
+            <Button size="small" href={deploy} target="_blank">Deploy</Button>
+        </CardActions>
+  </Card>
   )
 }
