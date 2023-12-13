39597e42f96ee86b7ec98ae7341e6b18370e93f7
RomiRao <raoromina96@gmail.com>
2023-12-12 23:55:09 -0300

    adding a skeleton to this beautiful project

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,65 @@
-import { BrowserRouter } from 'react-router-dom';
-import Footer from './components/Footer.jsx'
-import Navbar from './components/Navbar/Navbar.jsx'
-import Main from './components/Main/Main.jsx'
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Main from "./components/Main/Main.jsx";
 import { useState } from "react";
-import { createTheme, CssBaseline, ThemeProvider, Box } from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider, Box } from "@mui/material";
 
-import FolderSharedIcon from '@mui/icons-material/FolderShared';
-import CallIcon from '@mui/icons-material/Call';
-import HomeIcon from '@mui/icons-material/Home';
+import FolderSharedIcon from "@mui/icons-material/FolderShared";
+import CallIcon from "@mui/icons-material/Call";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function App() {
+    const navLinks = [
+        {
+            title: "Projects",
+            path: "/Projects",
+            icon: <FolderSharedIcon />,
+        },
+        {
+            title: "Contact",
+            path: "/Contact",
+            icon: <CallIcon />,
+        },
+        {
+            title: "Home",
+            path: "/",
+            icon: <HomeIcon />,
+        },
+    ];
 
-  
-  const navLinks = [
-    {
-      title:'Projects', path:'/Projects', icon: <FolderSharedIcon/ >
-    },
-    {
-      title:'Contact', path:'/Contact', icon: <CallIcon/ >
-    },
-    {
-      title:'Home', path:'/', icon: <HomeIcon/ > 
-    },
-  ]
+    const [mode, setMode] = useState(false);
 
+    const theme = createTheme({
+        palette: {
+            mode: mode ? "dark" : "light",
+        },
+    });
 
-  const [mode, setMode] = useState(false)
+    const [view, setView] = useState("Profile");
 
-  const theme = createTheme({
-    palette: {
-      mode: mode ? 'dark' : 'light'
-    },
-  });
-
-
-  const [view, setView] = useState('Profile')
-
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <CssBaseline />
-        <Box sx={{height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-          <Navbar setView={setView} setMode={setMode} mode={mode} navLinks={navLinks} />
-          <Main view={view} navLinks={navLinks} />
-          <Footer />
-        </Box>
-      </BrowserRouter>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <CssBaseline />
+                <Box
+                    sx={{
+                        height: "100vh",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <Navbar
+                        setView={setView}
+                        setMode={setMode}
+                        mode={mode}
+                        navLinks={navLinks}
+                    />
+                    <Main view={view} navLinks={navLinks} />
+                    <Footer />
+                </Box>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
-
